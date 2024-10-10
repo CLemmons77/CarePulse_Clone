7499e3a28f9bdf08e2b1676f059d2f84c8f3d9ce
CLemmons77 <caleblego7@gmail.com>
2024-10-09 22:26:26 -0400

    new patient collection id

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -62,7 +62,7 @@ const RegisterForm = ({ user }: { user: User }) => {
       // @ts-ignore
       const patient = await registerPatient(patientData)
 
-      if(patient) (router.push(`/patients/$(user.$id)/new-appointment`), console.log(patient))
+      if(patient) router.push(`/patients/$(user.$id)/new-appointment`)
     } catch (error) {
       console.log(error);
     }
